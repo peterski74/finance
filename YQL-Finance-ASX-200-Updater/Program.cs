e4d570d987c834b7491d52e3c1e0208019e9278f
peterski@gmail.com <lancer312>
2016-12-13 06:48:03 +1100

    first commit

--- YQL-Finance-ASX-200-Updater/Program.cs
+++ YQL-Finance-ASX-200-Updater/Program.cs
@@ -272,7 +272,7 @@ namespace YQL_Finance_ASX_200_Updater
                         myMail.SubjectEncoding = System.Text.Encoding.UTF8;
 
                         // set body-message and encoding
-                        myMail.Body = "<b>" + quote.Name.ToString() + " has dropped below 5% Today (" + quote.ChangePercent + ") </b>.";
+                        myMail.Body = "<b>" + quote.Name.ToString() + " has dropped below 5% Today (" + quote.ChangePercent.ToString() + ") </b>.";
                         myMail.BodyEncoding = System.Text.Encoding.UTF8;
                         // text or html
                         myMail.IsBodyHtml = true;
